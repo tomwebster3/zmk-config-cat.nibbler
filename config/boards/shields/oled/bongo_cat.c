976dc68ba99f3e287051c9aabb16a42738d4fec2
tomwebster3 <tomwapowapo@gmail.com>
2025-12-02 23:13:00 +0100

    Add ZMK config with OLED animation

---
+++ config/boards/shields/oled/bongo_cat.c
@@ -0,0 +1,10 @@
+#include "bongo.h"
+
+// Add your animation code here, for example a function to draw frames
+void draw_bongo_cat() {
+    // Example: you can loop through frames
+    for (int i = 0; i < BONGO_CAT_FRAME_COUNT; i++) {
+        // Code to send bongo_cat_frames[i] to OLED
+    }
+}
+
